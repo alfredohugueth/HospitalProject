8af6ab2e724ed2a31d6d35fe96abe6a49d4f7849
alfredohugueth <alfredohugueth99@gmail.com>
2020-10-31 18:45:03 -0500

    Correccion para organizacion

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+var cons = require('consolidate');
+const path = require('path');
+app.engine('html',cons.swig);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+    res.render('login');
+
+});
+app.on('listening', () => {
+	const address = app.address();
+	console.log(`server listening ${address.address}:${address.port}`);
+});
+
+app.listen('40000', function () {
+    console.log('Todo en orden');
+});
