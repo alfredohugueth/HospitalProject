ad1591a5090dd0268e0cb5c80c4238e16c1fd852
alfredohugueth <alfredohugueth99@gmail.com>
2020-10-31 19:16:56 -0500

    Modificaciones de experiencia de usuario

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ app.engine('html',cons.swig);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
+// Formulario de ingreso. Entrada predeterminada.
 app.get('/', function(req,res){
     res.render('login');
 
 });
+app.get('');
+// Aca iria la creacion de la info de la base de datos.
+
 app.on('listening', () => {
 	const address = app.address();
 	console.log(`server listening ${address.address}:${address.port}`);
