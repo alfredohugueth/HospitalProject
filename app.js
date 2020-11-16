5b872b76d852fe9cf9877a727adbd9e39b93d248
pmassonc <pmasson@uninorte.edu.co>
2020-11-16 00:05:03 -0500

    madministracion

--- app.js
+++ app.js
@@ -9,34 +9,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json({limit:'1mb'}));
 require('dotenv').config();
-// Base de datos
-const database = mysql.createConnection({
-    host: process.env.db_host, 
-    user: process.env.db_user , 
-    password: process.env.db_pass, 
-    database: process.env.db_data, 
-    port: process.env.db_port
-});
-database.connect((err,connection) =>{
-	if (err){
-		if (err.code === 'PROTOCOL_CONNECTION_LOST'){
-			console.error('CONEXION CON BASE DE DATOS PERDIDA');
-		}
-		if (err.code === 'ER_CON_COUNT_ERROR'){
-			 console.error('CONEXIONES OCUPADAS');
-		}
-	 if (err.code === 'ECONNREFUSED'){
-		 console.error('CONEXION RECHAZADA');
-	 }
-	 }
-	
-	 console.log('Base de datos conectada')
-});
-// Formulario de ingreso. Entrada predeterminada.
-app.get('/', function(req,res){
-    res.render('login');
+myConnection = require('express-myconnection');
 
-});
+app.set('view engine', 'ejs');
+// Base de datos
+
+
+app.use(myConnection(mysql, {
+	host: '',
+	user: '',
+	password: '',
+	port: 3306,
+	database: ''
+  }, 'single'));
+
+  app.use(express.urlencoded({extended: false}));
+
+
+
+// rutas
+const customerRoutes = require('./routes/customer');
+
+// routes
+app.use('/', customerRoutes);
+
+
 app.post('/',function(req,res){
     console.log(req);
     res.json({mensaje: "llego informacion del cliente"});
